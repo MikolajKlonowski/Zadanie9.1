c817f6c7074fdb2b01ed61715ee2cf1e291bb1ae
Mikołaj Klonowski <nick-93@o2.pl>
2018-06-16 12:35:57 +0200

    Zadanie 37 / 9.1 Zmiana wyjątków, z petla

--- src/Test.java
+++ src/Test.java
@@ -5,14 +5,23 @@ public class Test {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Podaj imie naziwko wiek i pesel osoby");
+        int i =1;
+      while (i ==1){
         try {
             Person person1 = new Person(sc.nextLine(), sc.nextLine(), sc.nextInt(), sc.nextInt());
             person1.printInfo();
+break;
+
         } catch (NameUndefinedException e){
+
             System.out.println("Podałeś za krótkie Imie lub Nazwisko, pamietaj że brak tych danych również powoduje ten komunikat");
-        } catch (IncorrectAgeException e){
+         i =1;
+         sc.nextLine();
+      } catch (IncorrectAgeException e){
             System.out.println("Podałeś zły wiek ");
-        }
+       i = 1;
+       sc.nextLine();
+      }}
 
 
     }
